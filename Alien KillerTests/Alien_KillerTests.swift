aa4547b9aa14055af3b0734216f254bb9b552242
Jaiveer Katariya <jvhurricane@Jaiveers-MacBook-Pro.local>
2015-04-14 09:59:14 -0700

    Initial Commit

---
+++ Alien KillerTests/Alien_KillerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Alien_KillerTests.swift
+//  Alien KillerTests
+//
+//  Created by Jaiveer Katariya on 4/14/15.
+//  Copyright (c) 2015 Jaiveer Katariya. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Alien_KillerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
